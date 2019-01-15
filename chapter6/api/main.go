b3fdea5306f5ae75190c72b107273c192618fc83
yosuke310 <yosuke310@gmail.com>
2019-01-15 09:27:01 +0900

    Fix typo

--- chapter6/api/main.go
+++ chapter6/api/main.go
@@ -48,7 +48,7 @@ func withData(d *mgo.Session, fn http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		thisDb := d.Copy()
 		defer thisDb.Close()
-		SetVar(r, "db", thisDb.DB("polls"))
+		SetVar(r, "db", thisDb.DB("ballots"))
 		fn(w, r)
 	}
 }
--- chapter6/api/polls.go
+++ chapter6/api/polls.go
@@ -1,15 +1,16 @@
 package main
 
 import (
-	"errors"
 	"net/http"
+
+	mgo "gopkg.in/mgo.v2"
 
 	"gopkg.in/mgo.v2/bson"
 )
 
 type poll struct {
 	ID      bson.ObjectId  `bson:"_id" json:"id"`
-	Title   string         `json:"titile"`
+	Title   string         `json:"title"`
 	Options []string       `json:"options"`
 	Results map[string]int `json:"results,omitempty"`
 }
